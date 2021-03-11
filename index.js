15183434a4ee22982bfe3da2a175da4a01375adb
Bé Na Cute <53372786+namanhishere@users.noreply.github.com>
2021-03-11 20:35:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -59,6 +59,19 @@ botcc.on("message", async message => {
         if(message.author.id === "227281028396351488") return message.reply("m khong nen sai bot nay vi chinh m da la nguoi chui bot nay ngu. thanks condi")
             message.reply("Xem tại: https://discord.com/channels/625715711481741322/625715711481741324/808710719531909129")
     }
+if(mesaage.content === "!!view"){
+if (!message.mentions.users.size) {
+		return message.channel.send(`Avatar của m: <${message.author.displayAvatarURL({ format: "png", dynamic: true })}>`);
+	}
+
+	const avatarList = message.mentions.users.map(user => {
+		return `${user.username}'s avatar: <${user.displayAvatarURL({ format: "png", dynamic: true })}>`;
+	});
+
+	// send the entire array of strings as a message
+	// by default, discord.js will `.join()` the array with `\n`
+	message.channel.send(avatarList);
+}
 })
 
 
